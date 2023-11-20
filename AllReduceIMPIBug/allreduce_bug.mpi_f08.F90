a9a4e7a9bab195312356abf7bc5f7c7c055355b6
Matthew Thompson <matthew.thompson@nasa.gov>
2023-11-20 14:40:05 -0500

    Add AllReduceIMPIBug

---
+++ AllReduceIMPIBug/allreduce_bug.mpi_f08.F90
@@ -0,0 +1,12 @@
+program test_allreduce_f08
+use mpi_f08
+implicit none
+
+integer :: status, rank
+
+call MPI_Init(ierror=status)
+call MPI_COMM_RANK(MPI_COMM_WORLD,rank,ierror=status)
+call MPI_ALLReduce(MPI_IN_PLACE,rank,1,MPI_INTEGER,MPI_MAX,MPI_COMM_WORLD,ierror=status)
+call MPI_Finalize(ierror=status)
+
+end program
